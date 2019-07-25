f06ed7c5c53e60afad43b807ca21b56053ecd555
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-25 13:43:45 +0200

    Conflicts fixed

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -11,11 +11,7 @@ const isIdvalid = (req, res, next) => {
 
 const isLoggedIn = (req, res, next) => {
   if (req.session.currentUser) {
-<<<<<<< HEAD
     return res.redirect('/t/home');
-=======
-    return res.redirect('/');
->>>>>>> ab710ff652367635ed7a091d0ee448297f235896
   }
   next();
 };
@@ -30,13 +26,10 @@ const isNotLoggedIn = (req, res, next) => {
 const isFormFilled = (req, res, next) => {
   const { username, password } = req.body;
   if (!username || !password) {
-<<<<<<< HEAD
     req.flash('ErrorFormFilled', 'Todos los campos son requeridos'); // mandamos al view el error
     if (username) {
       req.flash('dataForm', username);
     }
-=======
->>>>>>> ab710ff652367635ed7a091d0ee448297f235896
     return res.redirect(req.path);
   }
   next();
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,8 @@ const router = express.Router();
 const { isLoggedIn, isNotLoggedIn, isFormFilled, isIdvalid } = require('../middlewares/authMiddlewares');
 
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/home', isNotLoggedIn, (req, res, next) => {
   res.render('user/dashboard');
-=======
-router.get('/', isLoggedIn, (req, res, next) => {
-  res.render('auth/login');
->>>>>>> ab710ff652367635ed7a091d0ee448297f235896
 });
 
 module.exports = router;
