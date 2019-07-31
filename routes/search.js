7f1b12cf4a7d50761beaba20d5c1ad6dda8663cf
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-31 11:22:54 +0200

    Fixed search function

--- routes/search.js
+++ routes/search.js
@@ -7,29 +7,24 @@ const User = require('../models/User');
 router.get('/', async (req, res, next) => {
   const { username } = req.query;
   try {
-    const usersArray = await User.find({ username });
-    // console.log(usersArray);
-    res.locals.title = `Búsqueda de "${username}"`;
-    res.render('search', { usersArray });
+    User.find().then((usersArray) => {
+      const filteredUsers = usersArray.filter((user) => {
+        if (user.name.toLowerCase().includes(username.toLowerCase())) {
+          return user;
+        }
+        if (user.surnames.toLowerCase().includes(username.toLowerCase())) {
+          return user;
+        }
+        if (user.username.toLowerCase().includes(username.toLowerCase())) {
+          return user;
+        }
+      });
+      res.locals.title = `Búsqueda de "${username}"`;
+      res.render('search', { filteredUsers });
+    });
   } catch (err) {
     next(err);
   }
 });
-
-/*
-router.get('/', async (req, res, next) => {
-  const { username } = req.query;
-  if ({ username }.includes('a')) {
-    try {
-      const usersArray = await User.find({ username });
-      // console.log(usersArray);
-      res.locals.title = `Búsqueda de "${username}"`;
-      res.render('search', { usersArray });
-    } catch (err) {
-      next(err);
-    }
-  }
-});
-*/
 
 module.exports = router;
