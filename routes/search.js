570eb5ac94a687a32d24eaad0765bab26d0b74bf
prrrcl <adrianporcelnavarro@hotmail.com>
2019-08-01 12:52:27 +0200

    Added results style

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Status = require('../models/Status');
+const { cloudinary } = require('../config/cloud');
 
 /* GET home page. */
 
@@ -8,7 +10,7 @@ router.get('/', async (req, res, next) => {
   const { username } = req.query;
   try {
     const userLogged = await User.findOne({ _id: req.session.currentUser._id });
-    User.find().then((usersArray) => {
+    User.find().populate('status').then((usersArray) => {
       const filteredUsers = usersArray.filter((user) => {
         if (user.username === req.session.currentUser.username) {
           return null;
@@ -24,11 +26,19 @@ router.get('/', async (req, res, next) => {
         }
       });
       filteredUsers.forEach((usuario) => {
+        const lastState = usuario.status[usuario.status.length - 1].status;
+        usuario.lastState = lastState;
         if (userLogged.friends.includes(usuario._id)) {
           usuario.isFriend = true;
         }
+        const photo = usuario.profileImg;
+        const image = cloudinary.image(photo, { transformation: 'oneWithoutGrav', type: 'fetch' });
+        const splittedImage = image.split(' ');
+        const src = splittedImage[1];
+        const url = src.split("'")[1];
+        usuario.headPhoto = url;
       });
-      console.log(filteredUsers);
+
       res.locals.title = `Búsqueda de "${username}"`;
       res.render('search', { filteredUsers });
     });
